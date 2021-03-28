f7af81a97ca3b4a31578c01a025d895c3466514c
Norayr Gulyan <norayrgulyan@gmail.com>
2021-03-28 15:06:36 +0400

    docs : IImplementation

--- Assets/Update/IImplementation.cs
+++ Assets/Update/IImplementation.cs
@@ -3,10 +3,34 @@ namespace Update.System
 {
     public interface IImplementation
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="update">Interface IUpdate</param>
+        /// <param name="monoSwitch"></param>
+        /// <param name="scriptOrder"></param>
+        /// <param name="orderCount"></param>
+        /// <returns>True if Implementation is successfully integrated or removed/returns>
         bool Update(in IUpdate update,in MonoSwitch monoSwitch,in int scriptOrder = 0,in int orderCount = 0);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fixedUpdate">Interface IFixedUpdate</param>
+        /// <param name="monoSwitch"></param>
+        /// <param name="scriptOrder"></param>
+        /// <param name="orderCount"></param>
+        /// <returns>True if Implementation is successfully integrated or removed</returns>
         bool FixedUpdate(in IFixedUpdate fixedUpdate,in MonoSwitch monoSwitch,in int scriptOrder = 0,in int orderCount = 0);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lateUpdate">Interface ILateUpdate</param>
+        /// <param name="monoSwitch"></param>
+        /// <param name="scriptOrder"></param>
+        /// <param name="orderCount"></param>
+        /// <returns>True if Implementation is successfully integrated or removed</returns>
         bool LateUpdate(in ILateUpdate lateUpdate,in MonoSwitch monoSwitch,in int scriptOrder = 0,in int orderCount = 0);
     }
 }
